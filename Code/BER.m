78e7779b43086772996f915cad5168ad193012bb
Mickaël Misbach <mickael@misba.ch>
2015-12-16 01:55:18 +0100

    fix BER bug to work with both binary and multiclassification

--- Code/BER.m
+++ Code/BER.m
@@ -1,10 +1,22 @@
 function [ BER ] = BER( C, y, yHat )
 %BER Computes the Balanced Error Rate
 %   The balanced error rate is the average of the errors on each class
+%   Assumes classes are 1 to C
+
+% cast input to int, as this is a classification problem
+y = int8(y);
+yHat = int8(yHat);
+
+% range 0-based if binary, 1-based otherwise
+if C == 2 
+    range = 0:1;
+else
+    range = 1:C;
+end
 
 BER = 0;
 
-for c = 1:C
+for c = range
     currentClassMask = ones(length(y), 1) .* c;
     currentClassMask = currentClassMask == y;
     
